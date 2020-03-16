f3aa4101dab1ca8246d11ca5c4f1700523fe609a
jefffischer <jfischer@broadleafcommerce.org>
2020-03-16 02:03:10 -0500

    Support the new domain class case

--- ms-tutorial-new-entity-complete/src/main/java/com/broadleafsamples/tutorials/services/catalog/repository/RecipeRepository.java
+++ ms-tutorial-new-entity-complete/src/main/java/com/broadleafsamples/tutorials/services/catalog/repository/RecipeRepository.java
@@ -2,13 +2,11 @@ package com.broadleafsamples.tutorials.services.catalog.repository;
 
 import org.springframework.stereotype.Repository;
 
-import com.broadleafcommerce.data.tracking.core.TrackableRepository;
-import com.broadleafcommerce.data.tracking.core.TrackableRsqlFilterExecutor;
+import com.broadleafcommerce.data.tracking.core.BaseTrackableRepository;
 import com.broadleafcommerce.data.tracking.core.filtering.Narrow;
 import com.broadleafcommerce.data.tracking.jpa.filtering.narrow.JpaNarrowExecutor;
 import com.broadleafsamples.tutorials.services.catalog.provider.jpa.domain.JpaRecipe;
 
 @Repository
 @Narrow(JpaNarrowExecutor.class)
-public interface RecipeRepository extends TrackableRepository<JpaRecipe>,
-        TrackableRsqlFilterExecutor<JpaRecipe> {}
+public interface RecipeRepository extends BaseTrackableRepository<JpaRecipe> {}
--- ms-tutorial-new-entity-complete/src/main/java/com/broadleafsamples/tutorials/services/catalog/service/MyRecipeService.java
+++ ms-tutorial-new-entity-complete/src/main/java/com/broadleafsamples/tutorials/services/catalog/service/MyRecipeService.java
@@ -3,20 +3,17 @@ package com.broadleafsamples.tutorials.services.catalog.service;
 import org.springframework.stereotype.Service;
 
 import com.broadleafcommerce.common.extension.projection.Projection;
+import com.broadleafcommerce.data.tracking.core.BaseTrackableRepository;
 import com.broadleafcommerce.data.tracking.core.service.BaseRsqlCrudEntityService;
 import com.broadleafcommerce.data.tracking.core.service.RsqlCrudEntityHelper;
 import com.broadleafsamples.tutorials.services.catalog.provider.jpa.domain.JpaRecipe;
-import com.broadleafsamples.tutorials.services.catalog.repository.RecipeRepository;
 
 @Service
 public class MyRecipeService extends BaseRsqlCrudEntityService<Projection<JpaRecipe>> {
 
-    private final RecipeRepository repository;
-
-    public MyRecipeService(RecipeRepository repository,
+    public MyRecipeService(BaseTrackableRepository<JpaRecipe> repository,
             RsqlCrudEntityHelper helper) {
         super(repository, helper);
-        this.repository = repository;
     }
 
 }
