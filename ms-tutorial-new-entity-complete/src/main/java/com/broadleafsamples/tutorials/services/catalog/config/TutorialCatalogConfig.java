0a993d3f265d8e9de88fa062e09669fc6e53c7d8
jefffischer <jfischer@broadleafcommerce.org>
2020-03-16 16:34:40 -0500

    Introduce dynamic repository creation support.

--- ms-tutorial-new-entity-complete/src/main/java/com/broadleafsamples/tutorials/services/catalog/config/TutorialCatalogConfig.java
+++ ms-tutorial-new-entity-complete/src/main/java/com/broadleafsamples/tutorials/services/catalog/config/TutorialCatalogConfig.java
@@ -7,17 +7,23 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.broadleafcommerce.catalog.provider.jpa.autoconfigure.CatalogJpaAutoConfiguration;
 import com.broadleafcommerce.common.jpa.data.entity.JpaEntityScan;
+import com.broadleafcommerce.data.tracking.jpa.filtering.EnableJpaTrackableFlows;
 import com.broadleafcommerce.data.tracking.jpa.filtering.narrow.factory.JpaTrackableRepositoryFactoryBean;
-import com.broadleafsamples.tutorials.services.catalog.repository.RecipeRepository;
+import com.broadleafsamples.tutorials.services.catalog.provider.jpa.domain.JpaRecipe;
+import com.broadleafsamples.tutorials.services.catalog.repository.ProductRecipeRepository;
+
+import static com.broadleafcommerce.catalog.provider.jpa.Constants.Persistence.CATALOG_ROUTE_KEY;
+import static com.broadleafcommerce.catalog.provider.jpa.Constants.Persistence.CATALOG_ROUTE_PACKAGE;
 
 @ConditionalOnProperty(name = "broadleaf.database.provider", havingValue = "jpa")
 @Configuration
-@EnableJpaRepositories(basePackageClasses = RecipeRepository.class,
+@EnableJpaRepositories(basePackageClasses = ProductRecipeRepository.class,
         repositoryFactoryBeanClass = JpaTrackableRepositoryFactoryBean.class,
         entityManagerFactoryRef = "catalogEntityManagerFactory",
         transactionManagerRef = "catalogTransactionManager")
+@EnableJpaTrackableFlows(entityClasses = JpaRecipe.class, routeKey = CATALOG_ROUTE_KEY)
 @JpaEntityScan(basePackages = "com.broadleafsamples.tutorials.services.catalog.provider.jpa.domain",
-        routePackage = "com.broadleafcommerce.catalog")
+        routePackage = CATALOG_ROUTE_PACKAGE)
 @AutoConfigureAfter(CatalogJpaAutoConfiguration.class)
 public class TutorialCatalogConfig {
 }
