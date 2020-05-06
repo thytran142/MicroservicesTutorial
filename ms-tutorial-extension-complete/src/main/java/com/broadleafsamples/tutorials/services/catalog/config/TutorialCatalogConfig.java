2e07623d2fe48aae53f6bc9c52db1e63d10ca3f4
Michael Kloc <mkloc@broadleafcommerce.com>
2020-05-06 16:53:08 -0500

    * Split conceptual info out to new stubs * Move Endpoint tutorial to New Entity tutorial * Merge Service extension tutorial into Extended Entity Tutorial * Remove Export Extension Tutorial / Files

--- ms-tutorial-extension-complete/src/main/java/com/broadleafsamples/tutorials/services/catalog/config/TutorialCatalogConfig.java
+++ ms-tutorial-extension-complete/src/main/java/com/broadleafsamples/tutorials/services/catalog/config/TutorialCatalogConfig.java
@@ -4,10 +4,16 @@ import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.broadleafcommerce.catalog.dataexport.converter.ToStringConverter;
+import com.broadleafcommerce.catalog.domain.product.Product;
+import com.broadleafcommerce.catalog.domain.product.Variant;
 import com.broadleafcommerce.catalog.provider.jpa.autoconfigure.CatalogJpaAutoConfiguration;
+import com.broadleafcommerce.catalog.repository.product.ProductRepository;
+import com.broadleafcommerce.catalog.service.product.ProductService;
+import com.broadleafcommerce.catalog.service.product.VariantService;
 import com.broadleafcommerce.common.jpa.data.entity.JpaEntityScan;
-import com.broadleafsamples.tutorials.services.catalog.dataexport.converter.TutorialProductExportRowConverter;
+import com.broadleafcommerce.data.tracking.core.Trackable;
+import com.broadleafcommerce.data.tracking.core.service.RsqlCrudEntityHelper;
+import com.broadleafsamples.tutorials.services.catalog.service.TutorialProductService;
 
 @Configuration
 @JpaEntityScan(basePackages = "com.broadleafsamples.tutorials.services.catalog.provider.jpa.domain",
@@ -16,9 +22,11 @@ import com.broadleafsamples.tutorials.services.catalog.dataexport.converter.Tuto
 public class TutorialCatalogConfig {
 
     @Bean
-    public TutorialProductExportRowConverter customProductExportRowConverter(
-            ToStringConverter<Object> toStringConverter) {
-        return new TutorialProductExportRowConverter(toStringConverter);
+    public ProductService<Product> customProductService(
+        ProductRepository<Trackable> productRepository,
+        RsqlCrudEntityHelper helper,
+        VariantService<Variant> variantService) {
+        return new TutorialProductService(productRepository, helper, variantService);
     }
 
 }
