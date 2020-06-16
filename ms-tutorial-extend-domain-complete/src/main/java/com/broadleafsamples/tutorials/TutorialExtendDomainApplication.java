a4ee2724b240ca630d365418465402b7778fb151
Federico Wachs <fefewachs@gmail.com>
2020-06-16 17:46:49 -0300

    initial drop of the extended domain tutorial

---
+++ ms-tutorial-extend-domain-complete/src/main/java/com/broadleafsamples/tutorials/TutorialExtendDomainApplication.java
@@ -0,0 +1,13 @@
+package com.broadleafsamples.tutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialExtendDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialExtendDomainApplication.class, args);
+    }
+
+}
