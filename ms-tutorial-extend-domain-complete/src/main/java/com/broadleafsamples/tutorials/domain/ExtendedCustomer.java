68d820390101ba5bfe9fbd3adaef09c0be7dfeaf
Federico Wachs <fefewachs@gmail.com>
2020-06-18 10:38:39 -0300

    Added oneToMany relationship on extended customer

--- ms-tutorial-extend-domain-complete/src/main/java/com/broadleafsamples/tutorials/domain/ExtendedCustomer.java
+++ ms-tutorial-extend-domain-complete/src/main/java/com/broadleafsamples/tutorials/domain/ExtendedCustomer.java
@@ -6,7 +6,9 @@ import lombok.EqualsAndHashCode;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.Set;
 
 @Entity
 @Table(name = "TUTORIAL_CUSTOMER")
@@ -16,4 +18,7 @@ public class ExtendedCustomer extends JpaCustomer {
 
     @Column(name = "EXTERNAL_SOURCE_ID")
     private String externalSourceId;
+
+    @OneToMany(mappedBy="customer")
+    private Set<CustomerItem> items;
 }
