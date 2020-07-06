c01474b4e991d3590f37f3dde6da49a4b800f782
Federico Wachs <fefewachs@gmail.com>
2020-07-06 16:51:36 -0300

    added missing classes

--- ms-tutorial-movie-complete/src/main/java/com/broadleafsamples/tutorials/domain/Movie.java
+++ ms-tutorial-movie-complete/src/main/java/com/broadleafsamples/tutorials/domain/Movie.java
@@ -1,12 +1,59 @@
 package com.broadleafsamples.tutorials.domain;
 
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "movies")
 public class Movie {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    private String title;
-    private String synopsis
+    public String title;
+    private String synopsis;
     private Date releaseDate;
     private String director;
-    private Set<Actor> actors;
+//    private Set<Actor> actors;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
 }
--- ms-tutorial-movie-complete/src/main/java/com/broadleafsamples/tutorials/repository/MovieRepository.java
+++ ms-tutorial-movie-complete/src/main/java/com/broadleafsamples/tutorials/repository/MovieRepository.java
@@ -1,4 +1,13 @@
-package com.broadleafsamples.tutorials;
+package com.broadleafsamples.tutorials.repository;
 
-public class MovieRepository {
+import com.broadleafsamples.tutorials.domain.Movie;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "movies", path = "movies")
+public interface MovieRepository extends PagingAndSortingRepository<Movie, Long> {
+    List<Movie> findByTitle(@Param("title") String title);
 }
